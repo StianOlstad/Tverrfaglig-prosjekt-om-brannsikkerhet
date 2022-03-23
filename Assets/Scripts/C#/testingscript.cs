a5e762ebde28a466eed09d61ae5914b1a2c042e4
TricksterNO <marlped1605@gmail.com>
2022-03-23 14:10:29 +0100

    fire

---
+++ Assets/Scripts/C#/testingscript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testingscript : MonoBehaviour
+{
+    private void Start() {
+        Application.targetFrameRate = 30;
+    }
+}
