4c1b9bcd44aea36e0a249e8fbd2b483425004c37
TricksterNO <marlped1605@gmail.com>
2022-03-27 17:30:37 +0200

    FireSpread

--- Assets/Scripts/C#/StartFire_Script.cs
+++ Assets/Scripts/C#/StartFire_Script.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,18 +7,40 @@ public class StartFire_Script : MonoBehaviour
 {
     public string ignitionObject;
     public bool fireStarted;
+    public GameObject[] firePos;
+    [SerializeField] private float fireSpreadRate;
 
-    [SerializeField] private ParticleSystem _particles;
+    public int firesStarted;
+
+    private ParticleSystem _particles;
 
     private void Start() {
+        firePos = GameObject.FindGameObjectsWithTag("FireSpawn");
+        firePos = firePos.OrderBy((d) => (d.transform.position - transform.position).sqrMagnitude).ToArray();
+        for (int i = 0; i < firePos.Length; i++)
+        {
+            firePos[i].SetActive(false);
+        }
+
+        _particles = gameObject.GetComponent<ParticleSystem>();
         _particles.Stop();
     }
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == ignitionObject)
+        if (other.gameObject.tag == ignitionObject && fireStarted == false)
         {
             fireStarted = true;
             _particles.Play();
             Debug.Log("Fire Started");
+            StartCoroutine("FireSpread");
+        }
+    }
+
+    private IEnumerator FireSpread(){
+        yield return new WaitForSeconds(fireSpreadRate);
+        for (int i = 0; i < firePos.Length; i++)
+        {
+            firePos[i].SetActive(true);
+            yield return new WaitForSeconds(fireSpreadRate);
         }
     }
 }
