54342fd30cd3e30efb925238048e4853f101a5ee
TricksterNO <marlped1605@gmail.com>
2022-04-06 15:51:04 +0200

    saaaaaaaaaa

--- Assets/Scripts/C#/FireFireFire.cs
+++ Assets/Scripts/C#/FireFireFire.cs
@@ -4,7 +4,36 @@ using UnityEngine;
 
 public class FireFireFire : MonoBehaviour
 {
-    private void Awake() {
-        
+    public GameObject _ob;
+    [SerializeField] private string FireStarterObject;
+    [SerializeField] private LayerMask _layer;
+    private RaycastHit hitinfo;
+    private RaycastHit hit;
+    private bool OnFire;
+
+    private void Start() {
+        gameObject.GetComponent<ParticleSystem>().Stop();
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("1");
+        if (other.gameObject.tag == FireStarterObject && OnFire == false)
+        {
+            beginfire();
+        }
+    }
+
+    public void beginfire() {
+        Debug.Log("2");
+        gameObject.GetComponent<ParticleSystem>().Play();
+        OnFire = true;
+        Physics.SphereCast(transform.position, 15f, transform.position, out hit, 0.0f, _layer);
+        _ob = hit.collider.transform.gameObject;
+        if (hit.collider.GetComponent<GameObject>().tag == "Respawn")
+        {
+            Debug.Log("3");
+            hit.collider.GetComponent<GameObject>().GetComponent<FireFireFire>().beginfire();
+            
+        }
     }
 }
