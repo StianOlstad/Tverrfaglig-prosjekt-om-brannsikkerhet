c8ab4143eed50473190d985dd486d23713597430
StianOlstad <solstad1987@gmail.com>
2022-04-20 16:52:59 +0200

    End screen

---
+++ Assets/Scripts/C#/Credit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credit : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    
+}
