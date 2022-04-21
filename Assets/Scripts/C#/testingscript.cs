f21f7993b704af8522194878093cdf543f62b0eb
TricksterNO <marlped1605@gmail.com>
2022-04-21 22:15:34 +0200

    wads

--- Assets/Scripts/C#/testingscript.cs
+++ Assets/Scripts/C#/testingscript.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class testingscript : MonoBehaviour
 {
+    [SerializeField] private GameObject[] _ALLFIRESPAWNS;
     private void Start() {
         Application.targetFrameRate = 30;
+        _ALLFIRESPAWNS = GameObject.FindGameObjectsWithTag("FireSpawn");
     }
 }
