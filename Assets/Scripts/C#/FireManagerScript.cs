84757404e748f69a4ab1e2ef4757159d31b5ef13
TricksterNO <marlped1605@gmail.com>
2022-04-21 13:16:18 +0200

    Ovnen

--- Assets/Scripts/C#/FireManagerScript.cs
+++ Assets/Scripts/C#/FireManagerScript.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FireManagerScript : MonoBehaviour
 {
+    public int winScene;
+    public int losScene;
     [SerializeField] private Slider _slider;
     [SerializeField] private float OnFire;
     [SerializeField]private float FireObjects;
@@ -16,12 +19,20 @@ public class FireManagerScript : MonoBehaviour
     private void Update() {
         percentOfflame = OnFire / FireObjects * 100f;
         _slider.value= percentOfflame * 1f / 100f;
+
+        if (_slider.value == 1f)
+        {
+            victory();
+        }
     }
 
     public void StartedBurning(){
         OnFire += 1;
     }
+    private void victory(){
+        SceneManager.LoadScene(winScene);
+    }
     public void death(){
-        //Lose Scene
+        SceneManager.LoadScene(losScene);
     }
 }
